b77bf395c6960952a2ea463cc6fe643a3f2c933c
diggercode <68667145+diggercode@users.noreply.github.com>
2020-07-27 19:50:16 -0300

    Update categoria.js

--- assets/js/categoria.js
+++ assets/js/categoria.js
@@ -33,37 +33,3 @@ $(document).ready(function () {
 
 })
 
-
-
-$(function () {
-  num_posts_show = 10;
-  speed_to_top = 1000; // in ms
-  
-    $(".post").slice(0, num_posts_show).show();
-   
-    $("#loadmore").on('click', function (e) {
-        e.preventDefault();
-        $("article:hidden").slice(0, num_posts_show).slideDown();
-        if ($("article").length == 5) {
-            $("#load").fadeOut('slow');
-        }
-        $('html,body').animate({
-            scrollTop: $(this).offset().top
-        }, 1500);
-    });
-});
-
-$('#totop').click(function () {
-    $('body,html').animate({
-        scrollTop: 0
-    }, speed_to_top);
-    return false;
-});
-
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 50) {
-        $('#totop').fadeIn();
-    } else {
-        $('#totop').fadeOut();
-    }
-});
