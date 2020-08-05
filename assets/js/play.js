83bb3d7f9eaf42d9a232b84a91f7816fb7258376
diggercode <68667145+diggercode@users.noreply.github.com>
2020-08-05 20:23:24 -0300

    Update play.js

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,7 +1,7 @@
 function myFunction() {
     const c = document.querySelector('#code').value, 
     t = document.querySelector('#temp').value, e = document.querySelector('#ep').value, 
-    http = "http://busk.azureedge.net/tv", leng = document.querySelector('#lang').value;
+    http = "http://`braintv.azureedge.net/tv", leng = document.querySelector('#lang').value;
     document.querySelector("#informa").innerHTML = `Você Está Assistindo ao Episódio ${e}`
     document.getElementById("myFrame").src = `${http}/${c}/${t}/${leng}/${e}.mp4`
 }
